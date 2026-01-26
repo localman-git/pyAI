23b9feb31820baa08ae7829576c316bb5fe16df3
localman-git <efitton5491@gmail.com>
2026-01-26 08:46:51 +0000

    now accepts prompt from user

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 def main():
     
-    # Get gemini API key and pass to Google.
     load_dotenv()
     api_key = os.environ.get('GEMINI_API_KEY')
     if api_key == None:
@@ -12,16 +12,19 @@ def main():
     client = genai.Client(api_key=api_key)
 
     model = 'gemini-2.5-flash'
-    contents = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-    
-    response = client.models.generate_content(model=model,contents=contents)
+
+    parser = argparse.ArgumentParser(description='Chatbot')
+    parser.add_argument('user_prompt', type=str, help='User prompt')
+    args = parser.parse_args()
+        
+    response = client.models.generate_content(model=model,contents=args.user_prompt)
 
     if response.usage_metadata == None:
             raise RuntimeError('no usage metadata returned from api')
     if response.text == None:
             raise RuntimeError('no text returned from api')
     
-    print(f'User prompt: {contents}')
+    print(f'User prompt: {args.user_prompt}')
     print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
     print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
     print(f'Response: {response.text}')
