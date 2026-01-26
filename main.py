4d68fa12ffb5774277c76fb1b949cd4aef215940
localman-git <efitton5491@gmail.com>
2026-01-26 18:52:15 +0000

    --verbose flag added

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
 
     parser = argparse.ArgumentParser(description='Chatbot')
     parser.add_argument('user_prompt', type=str, help='User prompt')
+    parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -27,9 +28,11 @@ def main():
     if response.text == None:
             raise RuntimeError('no text returned from api')
     
-    print(f'User prompt: {args.user_prompt}')
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    if args.verbose == True:
+        print(f'User prompt: {args.user_prompt}')
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+        
     print(f'Response: {response.text}')
 
 if __name__ == "__main__":
