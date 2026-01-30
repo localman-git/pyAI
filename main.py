f76cacf36b8baafd20f6ee05de2a818351eef1ef
localman-git <efitton5491@gmail.com>
2026-01-30 19:32:53 +0000

    can now loop through calls

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -22,38 +23,49 @@ def main():
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    for _ in range(20):
         
-    response = client.models.generate_content(
-        model=model,
-        contents=messages,
-        config=types.GenerateContentConfig(
-            system_instruction=system_prompt,
-            tools=[available_functions]),
-        )
+        response = client.models.generate_content(
+            model=model,
+            contents=messages,
+            config=types.GenerateContentConfig(
+                system_instruction=system_prompt,
+                tools=[available_functions]),
+            )
 
-    if response.usage_metadata == None:
-        raise RuntimeError('no usage metadata returned from api')
-    if response.text == None and response.function_calls == None:
-        raise RuntimeError('no text or function calls returned from api')
-    
-    if args.verbose == True:
-        print(f'User prompt: {args.user_prompt}')
-        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+        if not response.usage_metadata:
+            raise RuntimeError('no usage metadata returned from api')
+        if not response.text and not response.function_calls:
+            raise RuntimeError('no text or function calls returned from api')
+        
+        if args.verbose:
+            print(f'User prompt: {args.user_prompt}')
+            print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+            print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
 
-    if response.function_calls != None:
-        function_results = []
-        for call in response.function_calls:
-            function_call_result = call_function(call, verbose=args.verbose)
-            if function_call_result.parts is None:
-                raise RuntimeError('no parts list returned from function call')
-            if function_call_result.parts[0].function_response.response is None:
-                raise RuntimeError('no response returned from function call')
-            function_results.append(function_call_result.parts[0])
-            if args.verbose:
-                print(f'-> {function_call_result.parts[0].function_response.response}')
+        if response.candidates:
+            for message in response.candidates:
+                messages.append(message.content)
+
+        if response.function_calls:
+            function_results = []
+            for call in response.function_calls:
+                function_call_result = call_function(call, verbose=args.verbose)
+                if not function_call_result.parts:
+                    raise RuntimeError('no parts list returned from function call')
+                if not function_call_result.parts[0].function_response.response:
+                    raise RuntimeError('no response returned from function call')
+                function_results.append(function_call_result.parts[0])
+                messages.append(types.Content(role='user', parts=function_results))
+                if args.verbose:
+                    print(f'-> {function_call_result.parts[0].function_response.response}')
+        else:     
+            print(f'Response: {response.text}')
+            break
+
     else:
-        print(f'Response: {response.text}')
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
